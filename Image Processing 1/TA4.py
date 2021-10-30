4511c2d81a0323c8d8c6ea60fd9219b5615f1795
Kiaeipour <a.kiaipoor@gmail.com>
2021-10-30 16:57:26 +0330

    image processing

--- Image Processing 1/TA4.py
+++ Image Processing 1/TA4.py
@@ -5,13 +5,8 @@ img = cv2.resize(img,(600,400))
 width,height = img.shape
 for i in range(width):
     for j in range(height):
-        if 250 > img[i,j] > 190 :
-            img[i,j]=190
-        elif 189 > img[i,j] > 130:
-            img[i,j] = 150
-        elif 129 > img[i,j] > 110:
-            img[i,j] = 120
-        else:img[i,j] = 0
+        if img[i,j] < 100 :
+            img[i,j]=0
         
 cv2.imshow("output-4",img)
 cv2.waitKey()
